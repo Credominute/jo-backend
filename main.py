e61870ef8f406cee12ea2ac2fefc6dc513010ea8
Jean-Yves Rosaye <jrosaye@hotmail.com>
2025-03-30 22:02:15 +0200

    Updating (warning, requirements). End of tests.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
 from api.order_api import OrderApi
 from api.ticket_api import TicketApi
@@ -8,28 +9,44 @@ from api.user_api import UserApi
 from api.auth import router as auth_router
 from src.config.database import engine, Base
 
-# Création des tables : utilisation d'une fonction
+""" Création des tables : utilisation d'une fonction :
+    On supprime/recrée la base de données pour les tests
 def drop_and_create_database():
-    """Supprime et recrée la base de données en environnement de tests"""
     Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine) """
 
-app = FastAPI()
+# Création des tables sans suppression des données existantes
+Base.metadata.create_all(bind=engine)
 
-# Exécution de la suppression/recréation de la base au démarrage (tests)
+""" Exécution de la suppression/recréation de la base au démarrage : 
+    contenu déprécié pour référence puis actuel recommandé/lifespan
 @app.on_event("startup")
 def startup_event():
     drop_and_create_database()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    drop_and_create_database()
+    yield
+    
+# Création de l'application FastAPI avec lifespan
+app = FastAPI(lifespan=lifespan)"""
+
+app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+# Test_CORS sans passer par l'authentification (route : /ping)
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
 
-#Initialisation des api User, Ticket et Order
+# Initialisation des api User, Ticket et Order
 user_api = UserApi()
 ticket_api = TicketApi()
 order_api = OrderApi()
